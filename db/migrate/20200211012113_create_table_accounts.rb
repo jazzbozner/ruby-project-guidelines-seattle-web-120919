7297eae3601e0175d3c8478b2e60caf5a4e1732f
github username <github email address>
2020-02-10 17:28:57 -0800

    Tables created

---
+++ db/migrate/20200211012113_create_table_accounts.rb
@@ -0,0 +1,8 @@
+class CreateTableAccounts < ActiveRecord::Migration[5.2]
+  def change
+    create_table(:accounts) do |t|
+      t.integer :user_id
+      t.integer :website_id
+    end
+  end
+end
